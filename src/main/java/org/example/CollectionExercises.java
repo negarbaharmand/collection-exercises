37aea847be0bee93fbda0e32654aebf090665c73
Negar Baharmand <negarbaharmand@outlook.com>
2023-08-21 21:39:41 +0200

    Exercise 4-6

--- src/main/java/org/example/CollectionExercises.java
+++ src/main/java/org/example/CollectionExercises.java
@@ -1,16 +1,18 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class CollectionExercises {
     public static void main(String[] args) {
-        ex1();
-        ex2();
-        ex3();
+        //ex1();
+        //ex2();
+        //ex3();
+        //ex4();
+        //ex5();
+        ex6();
     }
-    public static void ex1(){
+
+    public static void ex1() {
         List<String> daysOfWeek = new ArrayList<String>();
         daysOfWeek.add("Monday");
         daysOfWeek.add("Tuesday");
@@ -21,7 +23,8 @@ public class CollectionExercises {
         daysOfWeek.add("Sunday");
         System.out.println("Days of the week: " + daysOfWeek);
     }
-    public static void ex2(){
+
+    public static void ex2() {
         List<String> daysOfWeek = new ArrayList<>();
         daysOfWeek.add("Monday");
         daysOfWeek.add("Tuesday");
@@ -38,7 +41,8 @@ public class CollectionExercises {
             System.out.println(day);
         }
     }
-    public static void ex3(){
+
+    public static void ex3() {
         List<String> daysOfWeek = new ArrayList<>();
         daysOfWeek.add("Monday");
         daysOfWeek.add("Tuesday");
@@ -55,16 +59,57 @@ public class CollectionExercises {
             if (!day.equals("Thursday")) {
                 System.out.println(day);
             }
-            if(day.equals("Thursday")){
+            if (day.equals("Thursday")) {
                 iterator.remove();
             }
         }
         daysOfWeek.add(3, "Thursday");
         System.out.println("Updated days of the week: ");
         for (String day : daysOfWeek
-             ) {
+        ) {
             System.out.println(day);
         }
     }
 
+    public static void ex4() {
+        List<String> daysOfWeek = new ArrayList<>();
+        daysOfWeek.add("Monday");
+        daysOfWeek.add("Tuesday");
+        daysOfWeek.add("Wednesday");
+        daysOfWeek.add("Thursday");
+        daysOfWeek.add("Friday");
+        daysOfWeek.add("Saturday");
+        daysOfWeek.add("Sunday");
+        List<String> firstThreeDays = daysOfWeek.subList(0, 3);
+        System.out.println("First three days of the week: " + firstThreeDays);
+
+    }
+
+    public static void ex5() {
+        Set<String> daysOfWeekSet = new HashSet<>();
+        daysOfWeekSet.add("Monday");
+        daysOfWeekSet.add("Tuesday");
+        daysOfWeekSet.add("Wednesday");
+        daysOfWeekSet.add("Thursday");
+        daysOfWeekSet.add("Friday");
+        daysOfWeekSet.add("Saturday");
+        daysOfWeekSet.add("Sunday");
+        System.out.println("Days of the week set: " + daysOfWeekSet);
+    }
+
+    public static void ex6() {
+        Set<String> daysOfWeekSet = new HashSet<>();
+
+        daysOfWeekSet.add("Monday");
+        daysOfWeekSet.add("Tuesday");
+        daysOfWeekSet.add("Wednesday");
+        daysOfWeekSet.add("Thursday");
+        daysOfWeekSet.add("Friday");
+        daysOfWeekSet.add("Saturday");
+        daysOfWeekSet.add("Sunday");
+
+        List<String> daysOfWeekList = new ArrayList<>(daysOfWeekSet);
+
+        System.out.println("ArrayList Days of the week : " + daysOfWeekList);
+    }
 }
